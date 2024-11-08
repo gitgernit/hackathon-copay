cbf2a4f25c1a96b31a97c1924d8f4b08e83ac4e6
timka <git@timka.su>
2024-11-08 20:27:31 +0300

    add name fiels to group

--- backend/app/models/group.py
+++ backend/app/models/group.py
@@ -7,9 +7,11 @@ from .user import User
 
 class Group(SQLModel, table=True):
     id: UUID = Field(primary_key=True, default_factory=uuid4)
+    name: str = Field(nullable=False, default="Change name pls")
     owner: User = Relationship(back_populates="groups")
 
     member: list['User'] = Relationship(back_populates="groups")
 
     async def add_member(self, member: User):
         self.member.append(member)
+        
