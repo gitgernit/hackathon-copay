203c067ad960ae3e813e1f2f361f997709ef9aa2
timka <git@timka.su>
2024-11-08 20:24:41 +0300

    add ofd string parser

---
+++ backend/app/utils/nalog.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from aiohttp import ClientSession
+from urllib.parse import parse_qs
+
+
+async def get_nalog_data(ofd_string: str) -> dict:
+    async with ClientSession() as session:
+        items = {k: v[0] for k, v in parse_qs(ofd_string).items()}
+
+        url = "https://ofd.ru/api/partner/v3/receipts/GetReceipt"
+        data = {
+            "TotalSum": items['s'],
+            "DocDateTime": datetime.strptime(items['t'], "%Y%m%dT%H%M%S").isoformat(),
+            "FnNumber": items['fn'],
+            "ReceiptOperationType": 1,
+            "DocNumber": items['n'],
+            "DocFiscalSign": items['i'],
+            "TokenSecret": "q23123" # todo change this
+        }
+        response = await session.post(
+            url=url,
+            json=data
+        )
+    if response.status != 200:
+        return {}
+    return await response.json()
+    
