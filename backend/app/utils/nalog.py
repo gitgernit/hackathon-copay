2dd778c5bf3df14966e388040d7ac4fe9715cf03
timka <git@timka.su>
2024-11-09 00:19:40 +0300

    add ofd check endpoint

--- backend/app/utils/nalog.py
+++ backend/app/utils/nalog.py
@@ -1,24 +1,11 @@
-from datetime import datetime
-from urllib.parse import parse_qs
-
 from aiohttp import ClientSession
 
 
 async def get_nalog_data(ofd_string: str) -> dict:
     async with ClientSession() as session:
-        items = {k: v[0] for k, v in parse_qs(ofd_string).items()}
-
-        url = 'https://ofd.ru/api/partner/v3/receipts/GetReceipt'
+        url = 'https://proverkacheka.com/api/v1/check/get'
         data = {
-            'TotalSum': items['s'],
-            'DocDateTime': datetime.strptime(
-                items['t'], '%Y%m%dT%H%M%S'
-            ).isoformat(),
-            'FnNumber': items['fn'],
-            'ReceiptOperationType': 1,
-            'DocNumber': items['n'],
-            'DocFiscalSign': items['i'],
-            'TokenSecret': 'q23123',  # todo change this
+            "qrraw": ofd_string
         }
         response = await session.post(url=url, json=data)
     if response.status != 200:
