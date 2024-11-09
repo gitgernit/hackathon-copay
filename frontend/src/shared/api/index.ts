cb599b0006dfd58f92ca56fe968e9099d40270d9
zethange <maksselles@gmail.com>
2024-11-09 10:48:16 +0300

    fix: update codegen

--- frontend/src/shared/api/index.ts
+++ frontend/src/shared/api/index.ts
@@ -1,7 +1,9 @@
-import { Configuration, DefaultApi } from "./generated";
+import { Configuration, AuthApi, GroupsApi, UtilsApi } from "./generated";
 
 export const c = new Configuration({
   basePath: import.meta.env.VITE_API_URL,
 });
 
-export const api = new DefaultApi(c);
+export const authApi = new AuthApi(c)
+export const groupsApi = new GroupsApi(c)
+export const utilsApi = new UtilsApi(c)
