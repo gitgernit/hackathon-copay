77f6d98d0711265141ec6ffc7b5dcd0154022218
gitgernit <petr.zakharov.anton@gmail.com>
2024-11-09 12:28:17 +0300

    fix(backend): fix migration from Group to Event

--- backend/app/api/events/routers.py
+++ backend/app/api/events/routers.py
@@ -2,8 +2,8 @@ import fastapi
 
 from .invites.routers import events_router
 
-groups_router = fastapi.APIRouter()
+events_router = fastapi.APIRouter()
 
-groups_router.include_router(
+events_router.include_router(
     events_router, prefix='/invites', tags=['invites']
 )
