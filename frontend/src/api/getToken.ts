c91cf007d2e44aa718b221f4e78d153ed6a52247
zethange <maksselles@gmail.com>
2024-11-09 14:22:00 +0300

    fix: auth in frontend

--- frontend/src/api/getToken.ts
+++ frontend/src/api/getToken.ts
@@ -4,11 +4,13 @@ import { TelegramInputData } from "@/shared/api/generated"
 export const getToken = async (telegramData: TelegramInputData) => {
   if (telegramData === null || telegramData === undefined) return 'No data'
 
-  const res = await authApi.authenticateApiAuthTokenPost(telegramData as any)
-  const data = res.accessToken
+  const res = await authApi.authenticateApiAuthTokenPostRaw({
+    telegramInputData: telegramData,
+  })
+  const data = await res.value()
 
   if (data === undefined) return 'No data'
   
-  localStorage.setItem('token', data)
-  return data
+  localStorage.setItem('token', data.accessToken)
+  return data.accessToken
 }
