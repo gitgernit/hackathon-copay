e765e2b7c3de7b57163b77884ef3afdcef4457f4
romavsterg <romann.mikhaylov@gmail.com>
2024-11-10 02:15:11 +0300

    feat: feat

--- frontend/src/Components/GroupList/GroupList.tsx
+++ frontend/src/Components/GroupList/GroupList.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import GroupItem from '../GroupItem/GroupItem'
 import './GroupList.css'
 import {useQuery} from "@tanstack/react-query";
 import {defaultReq, eventsApi} from "../../shared/api";
+import { useLocation } from 'react-router-dom';
 
 const GroupList = () => {
-  const {data} = useQuery({
+  const location = useLocation()
+
+  const {data, refetch} = useQuery({
     queryKey: ['events'],
     queryFn: () => eventsApi.listEventsApiEventsGet(defaultReq),
+    initialData: []
   })
 
-  // console.log(data)
+  useEffect(() => {
+    refetch()
+  }, [location])
+
+  console.log('data', data)
   
   return (
     <div className='groups'>
-        {data?.map((group) => (
+        {data?.length ? (data?.map((group) => (
             <GroupItem key={group.id} event={group} />
-        ))}
+        ))) : (<GroupItem key={data?.id} event={data} />)
+      }
     </div>
   )
 }
