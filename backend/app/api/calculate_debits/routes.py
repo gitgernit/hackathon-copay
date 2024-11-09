1b75f8c0dc7b2280ce52e51195abcfb85a3582f1
ITQ <itq.dev@ya.ru>
2024-11-09 23:23:38 +0300

    fix: fixed calculate debits

--- backend/app/api/calculate_debits/routes.py
+++ backend/app/api/calculate_debits/routes.py
@@ -1,60 +1,79 @@
 from uuid import UUID
+import pydantic
+import typing
 
-from fastapi import Depends
-from fastapi import HTTPException
-import httpx
-from models.event import Event
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
+import requests
+from sqlmodel import Session, select
+from fastapi import Depends, HTTPException
+
+from app.api.auth.deps import BearerAuth, get_current_user
+from app.core.db import engine
+from app.models.event import Event
+from app.models.user import User
 
 from .routers import calculate_debits_router
-from app.api.auth.deps import BearerAuth
+
+
+class Debt(pydantic.BaseModel):
+    from_user_id: int
+    to_user_id: int
+    amount: float
 
 
 @calculate_debits_router.post(
-    '/events/{event_id}/calculate-debts',
-    description='Get debts smeta',
+    "/events/{event_id}",
+    description="Get debts smeta",
     dependencies=[Depends(BearerAuth())],
 )
-async def calculate_event_debts(
-    event_id: UUID,
-    user: typing.Annotated[
-        User, fastapi.Depends(app.api.auth.deps.get_current_user)
-    ],
-    session: AsyncSession = Depends(get_session),
-):
-    result = await session.execute(select(Event).where(Event.id == event_id))
-    event = result.scalars().first()
+def calculate_event_debts(
+    event_id: UUID, user: User = Depends(get_current_user)
+) -> typing.List[Debt]:
+    with Session(engine) as session:
+        result = session.execute(select(Event).where(Event.id == event_id))
+        event = result.scalars().first()
 
-    if not event:
-        raise HTTPException(status_code=404, detail='Event not found')
+        if not event:
+            raise HTTPException(status_code=404, detail="Event not found")
 
-    participants = [{'id': user.id, 'name': user.name} for user in event.users]
+        transactions = []
+        for transaction in event.transactions:
+            if not transaction.closed:
+                positions = [
+                    {
+                        "price": item.price,
+                        "assigned_to_ids": [
+                            user.id for user in item.assigned_to
+                        ],
+                    }
+                    for item in transaction.items
+                ]
+                transactions.append(
+                    {"owner_id": transaction.payer_id, "positions": positions}
+                )
 
-    transactions = []
-    for transaction in event.transactions:
-        positions = [
-            {
-                'price': item.price,
-                'assigned_to_ids': [user.id for user in item.assigned_to],
-            }
-            for item in transaction.items
+                transaction.closed = True
+
+        if not transactions:
+            return []
+
+        participants = [
+            {"id": user.id, "name": user.name} for user in event.users
         ]
-        transactions.append(
-            {'owner_id': transaction.payer_id, 'positions': positions}
-        )
+        input_data = {
+            "participants": participants, "transactions": transactions,
+        }
 
-    input_data = {'participants': participants, 'transactions': transactions}
-    print(input_data)
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            'http://optimizetka:3000/optimizetka/api/calculate-debts',
+        response = requests.post(
+            "http://optimizetka:3000/optimizetka/api/calculate-debts",
             json=input_data,
         )
+
         if response.status_code != 200:
             raise HTTPException(
                 status_code=response.status_code,
-                detail='Failed to calculate debts',
+                detail=f"Failed to calculate debts: {response.text}",
             )
 
-    return response.json()
+        session.commit()
+
+        return response.json()
