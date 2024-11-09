a0e077cdca6d3d184ec6a74c5e3e9eb1023fdb37
gitgernit <petr.zakharov.anton@gmail.com>
2024-11-09 07:38:56 +0300

    feat(backend): POST /groups/invite - create invitation route

--- backend/app/api/groups/routes.py
+++ backend/app/api/groups/routes.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException
 import typing
 import uuid
 
@@ -8,6 +9,7 @@ import app.api.auth.deps
 from app.api.groups.routers import groups_router
 import app.core.db
 import app.models.group
+import app.models.invite
 from app.models.user import User
 
 
@@ -30,14 +32,41 @@ def list_groups(
 )
 def create_group(
     group: app.models.group.BaseGroup,
-    user: typing.Annotated[User, fastapi.Depends(app.api.auth.deps.get_current_user)],
+    user: typing.Annotated[
+        User, fastapi.Depends(app.api.auth.deps.get_current_user)
+    ],
 ) -> app.models.group.Group:
     with sqlmodel.Session(app.core.db.engine) as session:
-        new_group = app.models.group.Group(name=group.name, owner=user, users=[user])
+        new_group = app.models.group.Group(
+            name=group.name, owner=user, users=[user]
+        )
         session.add(new_group)
         session.commit()
 
     return new_group
+
+
+@groups_router.post(
+    '/invite',
+    dependencies=[fastapi.Depends(app.api.auth.deps.get_current_user)],
+    description='Create invite',
+)
+def create_invite(
+    data: app.models.invite.InputInvite,
+) -> app.models.invite.Invite:
+    with sqlmodel.Session(app.core.db.engine) as session:
+        group = session.get(app.models.group.Group, data.group_id)
+
+        if not group:
+            raise HTTPException(fastapi.status.HTTP_400_BAD_REQUEST)
+
+        invite = app.models.invite.Invite(
+            group=group,
+            usages=data.usages,
+            expiration_time=data.expiration_time,
+        )
+
+    return invite
 
 
 @groups_router.delete(
