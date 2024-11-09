bbb6e8eb298fc8f3e80be4a3468e9ff26c8d53d6
timka <git@timka.su>
2024-11-09 14:30:42 +0300

    fix auth

--- backend/app/models/telegram.py
+++ backend/app/models/telegram.py
@@ -6,23 +6,23 @@ from pydantic import BaseModel
 class TelegramWebUser(BaseModel):
     id: int
     first_name: str
-    last_name: str | None
-    username: str | None
-    language_code: str | None
+    last_name: str | None = ""
+    username: str | None = "" 
+    language_code: str | None = ""
     is_premium: bool = False
     added_to_attachment_menu: bool = False
     allows_write_to_pm: bool = False
-    photo_url: str | None
+    photo_url: str | None = ""
 
 
 class TelegramInputData(BaseModel):
-    query_id: str | None
+    query_id: str | None = ""
     user: TelegramWebUser
-    receiver: Any
-    chat: Any
-    chat_type: str | None
-    chat_instance: str | None
-    start_param: str | None
-    can_send_after: int | None
+    receiver: Any = ""
+    chat: Any = ""
+    chat_type: str | None = ""
+    chat_instance: str | None = ""
+    start_param: str | None = ""
+    can_send_after: int | None = ""
     auth_date: int
     hash: str
