9eb3098a2e5626d2edf4ebd61536c03eb0a2b574
romavsterg <romann.mikhaylov@gmail.com>
2024-11-09 20:35:53 +0300

    feat: добавил кнопку возврата и валидацию

--- frontend/src/pages/CreateGroupPage.tsx
+++ frontend/src/pages/CreateGroupPage.tsx
@@ -1,7 +1,11 @@
 import '../styles/CreateGroup.css'
 import CreateGroup from '../Components/CreateGroup/CreateGroup';
+import { useNavigate } from 'react-router-dom';
+import { LucideArrowLeft } from 'lucide-react';
 
 const CreateGroupPage = () => {
+  const navigate = useNavigate()
+
   return (
     <div className='p-4'>
         <header className='create-header'>
@@ -9,6 +13,9 @@ const CreateGroupPage = () => {
         </header>
         <main className=''>
             <CreateGroup />
+            <button className='shadow-[1px_3px_7px_1px_rgba(34,_60,_80,_0.2)] absolute bottom-[40px] left-[40px] bg-[#ece6f0] active:bg-pink-200 p-4 rounded-2xl' onClick={() => navigate('/')}>
+              <LucideArrowLeft />
+          </button>
         </main>
     </div>
   )
