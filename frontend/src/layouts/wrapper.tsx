1dcf563e0a452bff378ebc05b03e3c5fc03cf2c2
zethange <maksselles@gmail.com>
2024-11-09 14:12:45 +0300

    fix: auth in frontend

--- frontend/src/layouts/wrapper.tsx
+++ frontend/src/layouts/wrapper.tsx
@@ -1,10 +1,14 @@
 import {Outlet} from "react-router-dom";
 import {useEffect} from "react";
+import { AuthToken } from "@/api/server";
+import {useInitData} from "@vkruglikov/react-telegram-web-app";
 
 export const Wrapper = () => {
+  const [initUnsafe] = useInitData();
+  
   useEffect(() => {
     (async () => {
-    
+      await AuthToken.getToken(initUnsafe as any)
     })()
   }, []);
   
