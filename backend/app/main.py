7db5d6609434675e64a306e75cbdece8958ac0c7
timka <git@timka.su>
2024-11-09 18:14:27 +0300

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
 import app.api.routers
 import app.core.db
 
 app_router = fastapi.FastAPI()
+app_router.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app_router.include_router(
     app.api.routers.api_router,
