f74a6c7e3059570cbed0e1fd0e85f7a3a03e64a9
ITQ <itq.dev@ya.ru>
2024-11-09 18:28:10 +0300

    fix: some fix

--- backend/app/api/routers.py
+++ backend/app/api/routers.py
@@ -9,13 +9,13 @@ import app.api.transactions.routers
 api_router = fastapi.APIRouter()
 
 api_router.include_router(
-    app.api.auth.routers.auth_router, prefix='/auth', tags=['auth']
+    app.api.auth.routers.auth_router, prefix="/auth", tags=["auth"]
 )
 api_router.include_router(
-    app.api.utils.routers.utils_router, prefix='/utils', tags=['utils']
+    app.api.utils.routers.utils_router, prefix="/utils", tags=["utils"]
 )
 api_router.include_router(
-    app.api.events.routers.events_router, prefix='/events', tags=['events']
+    app.api.events.routers.events_router, prefix="/events", tags=["events"]
 )
 api_router.include_router(
     app.api.transactions.routers.transactions_router,
