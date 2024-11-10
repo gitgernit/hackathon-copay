d29560669665cbcecc1eeb4a6aca4a03e788894f
zethange <maksselles@gmail.com>
2024-11-10 11:07:15 +0300

    add: dialog for calculate debts

--- frontend/src/pages/EventPage.tsx
+++ frontend/src/pages/EventPage.tsx
@@ -1,25 +1,26 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useQuery} from "@tanstack/react-query";
-import {useEffect, useState} from "react";
-import {Scanner} from "@yudiel/react-qr-scanner";
-import {eventsApi} from "../shared/api";
-import {CreateTransactionModal} from "../Components/CreateTransactionModal";
-import {BackButton} from "@vkruglikov/react-telegram-web-app";
-import {LucideArrowLeft, Share} from "lucide-react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { Scanner } from "@yudiel/react-qr-scanner";
+import { calculateDebitsApi, defaultReq, eventsApi } from "../shared/api";
+import { CreateTransactionModal } from "../Components/CreateTransactionModal";
+import { BackButton } from "@vkruglikov/react-telegram-web-app";
+import { LucideArrowLeft, Share } from "lucide-react";
 import TransactionsList from "../Components/TransactionsList/TransactionsList";
-import {Dialog, DialogContent} from "../shared/ui/dialog";
+import { Dialog, DialogContent } from "../shared/ui/dialog";
 import QRCode from 'qrcode'
-import {Input} from "../shared/ui/input";
-import {Button} from "../shared/ui/button";
+import { Input } from "../shared/ui/input";
+import { Button } from "../shared/ui/button";
 import '../styles/EventPage.css'
 import React from "react";
-import {ScannerModal} from "../Components/ScannerModal";
+import { ScannerModal } from "../Components/ScannerModal";
 
 export const EventPage = () => {
   const { id } = useParams();
   const [isScanOpen, setIsScanOpen] = useState(false);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [share, setShare] = useState(false);
+  const [showDebt, setShowDebt] = useState(false);
   const navigate = useNavigate()
 
   const { data, refetch } = useQuery({
@@ -32,21 +33,32 @@ export const EventPage = () => {
 
   useEffect(() => {
     (async () => {
-        // if (isNaN(Number(id))) navigate('/', {replace: true})
+      // if (isNaN(Number(id))) navigate('/', {replace: true})
     })()
   }, [])
-  
-  
+
+  const calculateDebt = async () => {
+    await calculateDebitsApi.calculateEventDebtsApiCalculateDebitsEventsEventIdPost({
+      eventId: id!,
+    }, defaultReq).catch();
+
+    setShowDebt(true)
+  }
+
+
   return (
     <div>
       <div className="px-2 py-4 mb-2">
         <h1 className="text-4xl font-bold">{data?.name}</h1>
       </div>
-      <div className="w-[360px] border ml-auto mr-auto border-#e3e3e3"></div>
+      <div className="w-full border ml-auto mr-auto border-#e3e3e3"></div>
+      <div className="m-2">
+        <button className="p-2 rounded-2xl bg-red-100" onClick={calculateDebt}>Посчитать долги</button>
+      </div>
       <div className="p-2 overflow-y-auto max-h-[80dvh] grid gap-2">
         <TransactionsList eventId={id!} />
       </div>
-      
+
       <div className="absolute bottom-2 left-5 right-5">
         <div className="flex justify-center gap-2 items-center">
           <button className='bg-[#ece6f0] active:bg-pink-200 p-4 rounded-2xl' onClick={() => navigate('/')}>
@@ -61,7 +73,7 @@ export const EventPage = () => {
           >
             Сканировать чек
           </button>
-          
+
           <button className="bg-[#ece6f0] active:bg-pink-200 p-4 rounded-2xl" onClick={async () => {
             setShare(!share)
             setTimeout(() => {
@@ -72,18 +84,24 @@ export const EventPage = () => {
           </button>
         </div>
       </div>
-      
+
       <ScannerModal isOpen={isScanOpen} onClose={() => setIsScanOpen(false)} onHandle={() => {
         refetch()
       }} eventId={id!} />
-      
-      <CreateTransactionModal 
-        isOpen={isOpenModal} 
+
+      <CreateTransactionModal
+        isOpen={isOpenModal}
         onClose={() => setIsOpenModal(false)}
-        eventId={id!}/>
-        
+        eventId={id!} />
+
       <BackButton onClick={() => navigate('/')}></BackButton>
-      
+
+      <Dialog open={showDebt} onOpenChange={() => setShowDebt(!showDebt)}>
+        <DialogContent>
+          <span className="text-xl">Долги посчитаны, коллекторы отправлены</span>
+        </DialogContent>
+      </Dialog>
+
       <Dialog open={share} onOpenChange={() => setShare(!share)}>
         <DialogContent>
           <canvas id="goida" className='min-w-64 min-h-64 mx-auto' />
